bea4beed4103bba811b6bb3978713189cc0f4186
Quercitron <eugene.ivanov@gmail.com>
2016-01-21 19:32:00 +0300

    remove bats

--- move.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-rem for FF
-copy /b D:\ws\home\chrome-ext.loc\www\strava\includes\main.js D:\ws\home\chrome-ext.loc\www\strava\data\includes\main.js  > nul
--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call move.bat
-rem call clear.bat
-
-SET VIRTUAL_ENV=D:\progs\firefox_sdk\
-cfx run -p C:\Users\EUGENE~1\APPLIC~1\Mozilla\Firefox\Profiles\PWMPLE~1.DEF\
