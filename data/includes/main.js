c27077edc8b47772bda633bea1649e25826761b0
Quercitron <eugene.ivanov@gmail.com>
2017-09-15 10:59:14 +0300

    some-fixes-and-add-ln-to-trkfile

--- data/includes/main.js
+++ data/includes/main.js
@@ -40,12 +40,142 @@ jQuery(function() {
     		});
     		*/
 
+    		var process = function(start_time, isAddLF) {
+    			var title = document.title;
+
+
+    			//https://www.strava.com/stream/segments/12246849?streams%5B%5D=latlng&streams%5B%5D=distance&streams%5B%5D=altitude&_=1467826323538
+
+    			var path = '/stream/';
+    			if (type == 'segments') {
+    				path += 'segments/';
+    			}
+
+                httpLoad(
+                	base_url+path+id+'?streams%5B%5D=latlng&streams%5B%5D=distance&streams%5B%5D=altitude&streams%5B%5D=time&streams%5B%5D=moving',
+
+                    function(html) {
+                    	var o = JSON.parse(html);
+
+                    	if (o && typeof o['latlng'] != 'undefined' && o['latlng'].length) {
+                    		var r = [];
+
+                    		var LF = !isAddLF ? '' : '\n';
+                    		var SPACE = !isAddLF ? '' : '  ';
+
+                    		r.push('<?xml version="1.0" encoding="UTF-8"?>'+LF);
+                    		r.push('<gpx version="1.1" creator="Exported from Strava via extension e-ivanov.ru"');
+                    		r.push('  xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd"');
+                    		r.push('  xmlns="http://www.topografix.com/GPX/1/1"');
+                    		r.push('  xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1"');
+                    		r.push('  xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'+LF);
+                    		r.push('<metadata><link href="http://e-ivanov.ru/projects/strava-export-gpx/"><text>Strava gpx export</text></link><time>'+(new Date().toISOString())+'</time></metadata>'+LF);
+                    		r.push('<trk>'+LF);
+                    		r.push('<name>'+title+'</name>'+LF);
+                    		r.push('<trkseg>'+LF);
+
+                    		var date = new Date();
+
+                    		var i, c = o['latlng'], l = c.length, e, alt, time;
+                    		for (i=0; i < l; i++) {
+                    			e = c[i];
+
+                    			alt = o['altitude'][i];
+                    			if (typeof alt == 'undefined') {
+                    				alt = 0;
+                    			}
+
+                    			time = !o['time'] ? i : o['time'][i];
+
+                    			if (typeof time == 'undefined') {
+                    				time = 0;
+                    			}
+                    			time = start_time + time*1000;
+                    			date.setTime(time);
+                    			time = date.toISOString();
+
+                        		r.push('<trkpt lon="'+e[1]+'" lat="'+e[0]+'">'+LF);
+                        		r.push(SPACE+'<ele>'+alt+'</ele>'+LF);
+                        		r.push(SPACE+'<time>'+time+'</time>'+LF);
+                        		r.push('</trkpt>'+LF);
+                    		}
+                    		r.push('</trkseg>'+LF);
+                    		r.push('</trk>'+LF);
+                    		r.push('</gpx>');
+
+                    		var s = r.join('');
+                    		r = null;
+
+
+                            var output = $('<output style="display:none"></output>').insertAfter(button);
+                            output = output[0];
+
+
+                            var cleanUp = function(a) {
+                                a.dataset.disabled = true;
+
+                                // Need a small delay for the revokeObjectURL to work properly.
+                                setTimeout(function() {
+                                    window.URL.revokeObjectURL(a.href);
+                                    $(a).remove();
+                                }, 1500);
+                            };
+
+                            var downloadFile = function(text, filename) {
+
+                				const MIME_TYPE = 'text/xml';
+
+                                var prevLink = output.querySelector('a');
+                                if (prevLink) {
+                                  window.URL.revokeObjectURL(prevLink.href);
+                                  output.innerHTML = '';
+                                }
+
+                                var bb = new Blob([text], {type: MIME_TYPE});
+
+                                var a = document.createElement('a');
+                                a.download = filename;
+                                a.href = window.URL.createObjectURL(bb);
+                                a.textContent = 'Download';
+
+                                a.dataset.downloadurl = [MIME_TYPE, a.download, a.href].join(':');
+                                a.draggable = true; // Don't really need, but good practice.
+                                a.classList.add('dragout');
+
+                                output.appendChild(a);
+
+                                setTimeout(function() {
+                                	a.click();
+
+                                }, 100);
+
+                                a.onclick = function(e) {
+                                  if ('disabled' in this.dataset) {
+                                    return false;
+                                  }
+
+                                  cleanUp(this);
+                                };
+                            };
+
+                	        downloadFile(s, "ride."+id+".gpx");
+                    	}
+                    },
+
+                    function() {
+                    	alert('Error load file');
+                    }
+                );
+    		};
+
+
+
     		button.bind('click', function(event) {
 	    		if (type == 'routes') {
 	    			return true;
 	    		}
 
-    			var title = document.title;
+	    		var isAddLF = !!event.ctrlKey;
 
                 httpLoad(
                 	base_url+'/activities/'+id+'/facebook_open_graph_metadata',
@@ -56,121 +186,14 @@ jQuery(function() {
                     	if (mm && typeof mm[1] != 'undefined') {
     				        var start_time = (new Date(mm[1])).getTime();
 
-    						//https://www.strava.com/stream/segments/12246849?streams%5B%5D=latlng&streams%5B%5D=distance&streams%5B%5D=altitude&_=1467826323538
-
-    						var path = '/stream/';
-    						if (type == 'segments') {
-    							path += 'segments/';
-    						}
-
-                            httpLoad(
-                            	base_url+path+id+'?streams%5B%5D=latlng&streams%5B%5D=distance&streams%5B%5D=altitude&streams%5B%5D=time&streams%5B%5D=moving',
-                                function(html) {
-                                	var o = JSON.parse(html);
-
-                                	if (o && typeof o['latlng'] != 'undefined' && o['latlng'].length) {
-                                		var r = [];
-
-                                		r.push('<?xml version="1.0" encoding="UTF-8"?>');
-                                		r.push('<gpx version="1.1" creator="Exported from Strava via extension e-ivanov.ru"');
-                                		r.push('  xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd"');
-                                		r.push('  xmlns="http://www.topografix.com/GPX/1/1"');
-                                		r.push('  xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1"');
-                                		r.push('  xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">');
-                                		r.push('<metadata><link href="http://e-ivanov.ru"><text>Strava gpx export</text></link><time>'+(new Date().toISOString())+'</time></metadata>');
-                                		r.push('<trk>');
-                                		r.push('<name>'+title+'</name>');
-                                		r.push('<trkseg>');
-
-                                		var date = new Date();
-
-                                		var i, c = o['latlng'], l = c.length, e, alt, time;
-                                		for (i=0; i < l; i++) {
-                                			e = c[i];
-
-                                			alt = o['altitude'][i];
-                                			if (typeof alt == 'undefined') {
-                                				alt = 0;
-                                			}
-
-                                			time = !o['time'] ? i : o['time'][i];
-
-                                			if (typeof time == 'undefined') {
-                                				time = 0;
-                                			}
-                                			time = start_time + time*1000;
-                                			date.setTime(time);
-                                			time = date.toISOString();
-
-                                    		r.push('<trkpt lon="'+e[1]+'" lat="'+e[0]+'">');
-                                    		r.push('<ele>'+alt+'</ele>');
-                                    		r.push('<time>'+time+'</time>');
-                                    		r.push('</trkpt>');
-                                		}
-                                		r.push('</trkseg>');
-                                		r.push('</trk>');
-                                		r.push('</gpx>');
-
-                                		var s = r.join('');
-                                		r = null;
-
-
-                                        var output = $('<output style="display:none"></output>').insertAfter(button);
-                                        output = output[0];
-
-
-                                        var cleanUp = function(a) {
-                                            a.dataset.disabled = true;
-
-                                            // Need a small delay for the revokeObjectURL to work properly.
-                                            setTimeout(function() {
-                                                window.URL.revokeObjectURL(a.href);
-                                                $(a).remove();
-                                            }, 1500);
-                                        };
-
-                                        var downloadFile = function(text, filename) {
-
-                            				const MIME_TYPE = 'text/xml';
-
-                                            var prevLink = output.querySelector('a');
-                                            if (prevLink) {
-                                              window.URL.revokeObjectURL(prevLink.href);
-                                              output.innerHTML = '';
-                                            }
-
-                                            var bb = new Blob([text], {type: MIME_TYPE});
-
-                                            var a = document.createElement('a');
-                                            a.download = filename;
-                                            a.href = window.URL.createObjectURL(bb);
-                                            a.textContent = 'Download';
-
-                                            a.dataset.downloadurl = [MIME_TYPE, a.download, a.href].join(':');
-                                            a.draggable = true; // Don't really need, but good practice.
-                                            a.classList.add('dragout');
-
-                                            output.appendChild(a);
-
-                                            setTimeout(function() {
-                                            	a.click();
-
-                                            }, 100);
-
-                                            a.onclick = function(e) {
-                                              if ('disabled' in this.dataset) {
-                                                return false;
-                                              }
-
-                                              cleanUp(this);
-                                            };
-                                        };
-
-                            	        downloadFile(s, "ride."+id+".gpx");
-                                	}
-                                }
-                            );
+    				        process(start_time, isAddLF);
                     	}
+                    },
+
+                    function() {
+                    	var start_time = (new Date()).getTime();
+
+   				        process(start_time, isAddLF);
                     }
                 );
 
